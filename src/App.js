13fdbb91f3bccea081c7385b88554e32ffb53e1e
RicardoSilva94 <ricardojgsilva94@gmail.com>
2024-04-24 00:27:27 +0100

    Começar a formatar a navbar do header.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import Button from 'react-bootstrap/Button';
+import AppHeader from './components/header';
+
 
 function App() {
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>{' '}
+      <header id="header">
+      <AppHeader/>
+      </header>
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,39 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import React from 'react';
+
+
+export default function AppHeader() {
+    return(
+            <Navbar expand="lg" className="bg-body-tertiary py-3">
+              <Container>
+                <Navbar.Brand href="#home">
+                <img
+            src={'images/logo.png'}
+            alt="Logo"
+            width="100"
+            height="40"
+            className="d-inline-block align-top"
+          />
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                  <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#link">Entrar</Nav.Link>
+                    <Nav.Link href="#link">Quem Somos</Nav.Link>
+                    <Nav.Link href="#link">Sócios</Nav.Link>
+                    <Nav.Link href="#link">Quotas</Nav.Link>
+                    <Nav.Link href="#link">Contactos</Nav.Link>
+                  </Nav>
+                </Navbar.Collapse>
+              </Container>
+            </Navbar>
+          );
+        }
+        
+    
+    
+
+
