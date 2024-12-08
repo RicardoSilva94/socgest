0c56df61087459475af05a5899c8865c9f08a4a8
RicardoSilva94 <ricardojgsilva94@gmail.com>
2024-12-08 01:04:47 +0000

    replace in generate quota modal, period input from text to select box

--- src/components/modals/generateQuotaModal.jsx
+++ src/components/modals/generateQuotaModal.jsx
@@ -4,9 +4,12 @@ import PropTypes from 'prop-types';
 import { FaList, FaCalendarAlt, FaEuroSign, FaPen } from 'react-icons/fa';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import format from 'date-fns/format';
+import { format, getYear, getMonth, setMonth } from 'date-fns';
+
 
 const GenerateQuotaModal = ({ show, handleClose, handleGenerateQuota }) => {
+  const currentYear = getYear(new Date());
+  const currentMonth = getMonth(new Date());
   const [quotaData, setQuotaData] = useState({
     tipo: 'Anual',
     periodo: '',
@@ -15,33 +18,45 @@ const GenerateQuotaModal = ({ show, handleClose, handleGenerateQuota }) => {
     data_pagamento: new Date()
   });
 
+  const meses = [
+    'Janeiro',
+    'Fevereiro',
+    'Março',
+    'Abril',
+    'Maio',
+    'Junho',
+    'Julho',
+    'Agosto',
+    'Setembro',
+    'Outubro',
+    'Novembro',
+    'Dezembro',
+  ];
+
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setQuotaData(prevState => ({ ...prevState, [name]: value }));
+    setQuotaData((prevState) => ({ ...prevState, [name]: value }));
   };
 
   const handleDateChange = (date) => {
-    // Convertendo a data para string no formato "YYYY-MM-DD"
     const formattedDate = format(date, 'yyyy-MM-dd');
-    setQuotaData(prevState => ({ ...prevState, data_pagamento: formattedDate }));
+    setQuotaData((prevState) => ({ ...prevState, data_pagamento: formattedDate }));
   };
 
   const handleSubmit = () => {
-    // Verifica se data_pagamento é uma string e tenta convertê-la em Date
-    const dataPagamento = typeof quotaData.data_pagamento === 'string'
+    const dataPagamento =
+      typeof quotaData.data_pagamento === 'string'
         ? new Date(quotaData.data_pagamento)
         : quotaData.data_pagamento;
 
-    // Formatar a data no formato YYYY-MM-DD
     const formattedQuotaData = {
-        ...quotaData,
-        data_pagamento: dataPagamento.toISOString().split('T')[0] // Formata a data como "YYYY-MM-DD"
+      ...quotaData,
+      data_pagamento: dataPagamento.toISOString().split('T')[0]
     };
 
     handleGenerateQuota(formattedQuotaData);
     console.log('Quota gerada:', formattedQuotaData);
-};
-
+  };
 
   return (
     <Modal show={show} onHide={handleClose} centered>
@@ -53,7 +68,9 @@ const GenerateQuotaModal = ({ show, handleClose, handleGenerateQuota }) => {
           <Form.Group controlId="formTipo">
             <Form.Label>Tipo</Form.Label>
             <InputGroup>
-              <InputGroup.Text><FaList /></InputGroup.Text>
+              <InputGroup.Text>
+                <FaList />
+              </InputGroup.Text>
               <Form.Control as="select" name="tipo" value={quotaData.tipo} onChange={handleChange}>
                 <option value="Anual">Anual</option>
                 <option value="Mensal">Mensal</option>
@@ -63,20 +80,44 @@ const GenerateQuotaModal = ({ show, handleClose, handleGenerateQuota }) => {
           <Form.Group controlId="formPeriodo">
             <Form.Label>Período</Form.Label>
             <InputGroup>
-              <InputGroup.Text><FaCalendarAlt /></InputGroup.Text>
-              <Form.Control
-                type="text"
-                placeholder={quotaData.tipo === 'Anual' ? 'Ano (e.g., 2024)' : 'Mês (e.g., Julho 2024)'}
-                name="periodo"
-                value={quotaData.periodo}
-                onChange={handleChange}
-              />
+              <InputGroup.Text>
+                <FaCalendarAlt />
+              </InputGroup.Text>
+              {quotaData.tipo === 'Anual' ? (
+                <Form.Control
+                  as="select"
+                  name="periodo"
+                  value={quotaData.periodo}
+                  onChange={handleChange}
+                >
+                  <option value="">Selecione o ano aplicável...</option>
+                  <option value={currentYear - 1}>{currentYear - 1}</option>
+                  <option value={currentYear}>{currentYear}</option>
+                  <option value={currentYear + 1}>{currentYear + 1}</option>
+                </Form.Control>
+              ) : (
+                <Form.Control
+                  as="select"
+                  name="periodo"
+                  value={quotaData.periodo}
+                  onChange={handleChange}
+                >
+                  <option value="">Selecione o mês aplicável...</option>
+                  {meses.map((mes, index) => (
+                    <option key={index} value={`${mes} ${currentYear}`}>
+                      {`${mes} ${currentYear}`}
+                    </option>
+                  ))}
+                </Form.Control>
+              )}
             </InputGroup>
           </Form.Group>
           <Form.Group controlId="formValor">
             <Form.Label>Valor</Form.Label>
             <InputGroup>
-              <InputGroup.Text><FaEuroSign /></InputGroup.Text>
+              <InputGroup.Text>
+                <FaEuroSign />
+              </InputGroup.Text>
               <Form.Control
                 type="text"
                 placeholder="Valor da quota"
@@ -89,7 +130,9 @@ const GenerateQuotaModal = ({ show, handleClose, handleGenerateQuota }) => {
           <Form.Group controlId="formDescricao">
             <Form.Label>Descrição</Form.Label>
             <InputGroup>
-              <InputGroup.Text><FaPen /></InputGroup.Text>
+              <InputGroup.Text>
+                <FaPen />
+              </InputGroup.Text>
               <Form.Control
                 type="text"
                 placeholder="Descrição da quota"
@@ -102,7 +145,9 @@ const GenerateQuotaModal = ({ show, handleClose, handleGenerateQuota }) => {
           <Form.Group controlId="formPrazoPagamento">
             <Form.Label>Prazo de Pagamento</Form.Label>
             <InputGroup>
-              <InputGroup.Text><FaCalendarAlt /></InputGroup.Text>
+              <InputGroup.Text>
+                <FaCalendarAlt />
+              </InputGroup.Text>
               <DatePicker
                 selected={new Date(quotaData.data_pagamento)}
                 onChange={handleDateChange}
